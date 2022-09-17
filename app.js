c8bd74f3baf5c098da326ab36fc184d16d7bad96
Tomas Valdes <tommynvoiz@gmail.com>
2022-09-17 15:17:19 -0500

    add names , live commentary and optimize code

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
 const display1 = document.querySelector('#score1');
 const display2 = document.querySelector('#score2');
+const name1 = document.querySelector('#name1');
+const name2 = document.querySelector('#name2');
 const player1 = document.querySelector('.btn-primary');
 const player2 = document.querySelector('.btn-secondary');
 const reset = document.querySelector('.btn-warning');
 const selectScore = document.querySelector('#score-select');
 
+
+let pName1 = prompt("Player-1 add 3 letter name");
+name1.textContent = pName1.toUpperCase();
+let pName2 = prompt("Player-2 add 3 letter name");
+name2.textContent = pName2.toUpperCase();
 let score1 = 0;
 let score2 = 0;
 let isGameOver = false;
 let winningScore = 0; 
 
 player1.addEventListener("click", function() {
-    if(!isGameOver){
+    if(!isGameOver && winningScore!=0){
         score1 += 1;
         if(score1 === winningScore){
             isGameOver = true;
             display1.classList.add('winner');
             display2.classList.add('loser');
+            player1.disabled = true;
+            player2.disabled = true;
         }
         display1.textContent = score1;
     }
 });
 player2.addEventListener("click", function() {
-    if(!isGameOver){
+    if(!isGameOver && winningScore!=0){
         score2 += 1;
         if(score2 === winningScore){
             isGameOver = true;
             display2.classList.add('winner');
             display1.classList.add('loser');
+            player1.disabled = true;
+            player2.disabled = true;
         }
         display2.textContent = score2;
     }
@@ -48,4 +59,6 @@ function resetB(){
     display1.classList.remove('loser');
     display2.classList.remove('winner');
     display2.classList.remove('loser');
+    player1.disabled = false;
+    player2.disabled = false;
 }
